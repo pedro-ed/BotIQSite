36042f82080a18c47866ce12b44be204b8855240
pedro eduardo <pedroadv991@gmail.com>
2021-03-16 18:17:20 -0300

    Desing

--- pages/Home/js/main.js
+++ pages/Home/js/main.js
@@ -1,9 +1,11 @@
-import {UpdateLucroDia} from "../../../private/services/Deshboard/Update.js"
+import {UpdateLucroDia,UpdateTable} from "../../../private/services/Deshboard/Update.js"
 import {AjustLink} from "../../../private/services/Links/Ajuste.js"
 import {sair} from '../../../private/auth/sair.js'
 import {ExitLooding,InitLooding} from '../../../private/UI/Loding/Loding.js'
 // Updates
 AjustLink()
+UpdateTable(true,"")
+
 
 UpdateLucroDia()
 $("#updatevalueToday").click(function (e) { 
@@ -16,4 +18,17 @@ $("#sair").click(function (e) {
     e.preventDefault();
     sair()
 });
+
+$("#inputDate").change(function (e) { 
+    e.preventDefault();
+    var value = e.target.value.split("-")
+    var ano = parseInt(value[0])
+    var mes = parseInt(value[1])
+    var dia = parseInt(value[2])
+    var data = `${dia}/${mes}/${ano}`
+    UpdateTable(false,data)
+});
+
+
+
 
--- private/services/Deshboard/Update.js
+++ private/services/Deshboard/Update.js
@@ -1,4 +1,6 @@
+import {ExitLooding,InitLooding} from "../../UI/Loding/Loding.js"
 export function UpdateLucroDia() {
+    
     var id = "LucroHoje"
     var date = new Date
     var hoje = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`
@@ -19,6 +21,52 @@ export function UpdateLucroDia() {
             });
             console.log(LucroTotal)
             document.getElementById(id).innerText = "R$ "+Math.round(LucroTotal*100)/100
+        }
+    });
+}
+
+export function UpdateTable(hoje,DataFilter) {
+    InitLooding("tableHistorico",true,1000,"grande",false)
+    
+    document.getElementById("table-body").innerHTML = ""
+    document.querySelector("#table-total").innerText = "R$ "+Math.round(0*100)/100
+    document.querySelector("#total_filtrado").innerText = "R$ "+Math.round(0*100)/100
+
+    var id = "LucroHoje"
+    var date = new Date
+    if (hoje) {
+        DataFilter = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`
+    }
+    document.querySelector("#totalFiltrado").innerText = `Total Filtrado - ${DataFilter}`
+    $.ajax({
+        type: "GET",
+        url: "https://apibots-937d3.firebaseio.com/Botmanager/Logs.json",
+        data: "",
+        dataType: "json",
+        success: function (response) {
+            var lucroTotal = 0
+            Object.keys(response).forEach(element => {
+                var data = response[element]["data"]
+                if (data == DataFilter) {
+                    var valRow = response[element]
+                    var Elrow = document.createElement("tr")
+                    lucroTotal += valRow["lucro"]
+                    var lucro = "R$ "+Math.round(valRow["lucro"]*100)/100
+                    var investiment = "R$ "+Math.round(valRow["investiment"]*100)/100
+                    var direcao = valRow["direcao"]=="C"?"Compra":"Venda"
+                    var listEl = [valRow["data"],direcao,valRow["hora"],investiment,valRow["resultado"],valRow["tipoConta"],lucro]
+                    listEl.forEach(e =>{
+                        var row  = document.createElement("td")
+                        row.innerText = e
+                        Elrow.appendChild(row)
+                    })
+                    document.getElementById("table-body").appendChild(Elrow)
+                    document.querySelector("#table-total").innerText = "R$ "+Math.round(lucroTotal*100)/100
+                    
+                    document.querySelector("#total_filtrado").innerText = "R$ "+Math.round(lucroTotal*100)/100
+                }
+            });
+
         }
     });
 }
